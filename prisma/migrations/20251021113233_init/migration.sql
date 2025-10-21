9948ecce07778387a3609ad8f9d4aa88e534b45b
nurulislamrimon <nurulislamrimon@gmail.com>
2025-10-21 18:16:14 +0600

    PRISMA_MIGRATE/DB_MIGRATION

---
+++ prisma/migrations/20251021113233_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `CustomDomain` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `hostname` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'pending',
+    `verification` VARCHAR(191) NULL,
+    `ssl_status` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `CustomDomain_hostname_key`(`hostname`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
